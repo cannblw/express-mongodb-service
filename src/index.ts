1cfeef230122292b1f651753ad7baa59cb3f8556
Edgar Chirivella <edgar.95.12@gmail.com>
2022-01-15 19:13:25 +0100

    Install and configure AWS deployment

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import serverless from 'serverless-http';
+
 dotenv.config();
 
 const app = express();
 const { PORT } = process.env;
 
-app.get('/', (req, res) => res.send('Express server working'));
+app.post('/', (req, res) => res.send('Express server working'));
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server is running at localhost:${PORT}`);
 });
+
+export default serverless(app);
