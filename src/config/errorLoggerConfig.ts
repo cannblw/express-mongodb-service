0bfe97cf427da21a602c0e2a303915e644db48eb
Edgar Chirivella <edgar.95.12@gmail.com>
2022-01-15 19:37:05 +0100

    Configure Winston logger

---
+++ src/config/errorLoggerConfig.ts
@@ -0,0 +1,14 @@
+import winston from 'winston';
+import expressWinston from 'express-winston';
+
+const errorLoggerConfig = expressWinston.errorLogger({
+  transports: [
+    new winston.transports.Console(),
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json(),
+  ),
+});
+
+export default errorLoggerConfig;
--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,19 @@ import dotenv from 'dotenv';
 
 import serverless from 'serverless-http';
 
+import loggerConfig from './config/loggerConfig';
+import errorLoggerConfig from './config/errorLoggerConfig';
+
 dotenv.config();
 
 const app = express();
 const { PORT } = process.env;
 
+app.use(loggerConfig);
+
 app.post('/', (req, res) => res.send('Express server working'));
+
+app.use(errorLoggerConfig);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
