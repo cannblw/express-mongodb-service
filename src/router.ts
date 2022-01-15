96eda235728727abb2dbff9d410a87287e591b86
Edgar Chirivella <edgar.95.12@gmail.com>
2022-01-16 00:13:53 +0100

    Validate errors in router

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,20 @@
 import express from 'express';
 
+import getValidationErrors from './extensions/validateBody';
+import SearchRequest from './dto/searchRequest';
+import TypedRequest from './types/typedRequest';
+import SearchResponse from './dto/searchResponse';
+import ErrorCode from './enum/errorCode';
+
 const router = express.Router();
 
-router.post('/', (req: express.Request, res: express.Response) => res.send('Express server working'));
+router.post('/', async (req: TypedRequest<SearchRequest>, res: express.Response) => {
+  const { body } = req;
+
+  const errors = await getValidationErrors(body);
+  if (errors != null) {
+    res.send(new SearchResponse(ErrorCode.INCORRECT_BODY_FORMAT, errors), );
+  }
+});
 
 export default router;
