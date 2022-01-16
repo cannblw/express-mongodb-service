aaeb5a212e451771b926a2f94dc69e34278a5781
Edgar Chirivella <edgar.95.12@gmail.com>
2022-01-16 01:20:17 +0100

    Add logic to controller

--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,31 @@
 import express from 'express';
+
 import SearchRequest from './dto/searchRequest';
 import TypedRequest from './types/typedRequest';
 import dtoValidationMiddleware from './middlewares/dtoValidationMiddleware';
+import serviceContainer from './services/serviceContainer';
+import SearchResponse from './dto/searchResponse';
+import ResponseCode from './enum/responseCode';
+import responseMessage from './messages/responseMessage';
 
 const router = express.Router();
 
 router.post('/', dtoValidationMiddleware(SearchRequest), async (req: TypedRequest<SearchRequest>, res: express.Response) => {
+  const { searchService } = serviceContainer;
   const { body } = req;
 
-  res.send({ok: "ok"});
+  const records = searchService.searchItems(
+                    body.startDate,
+                    body.endDate,
+                    body.minCount,
+                    body.maxCount);
+  
+  const response = new SearchResponse(
+                      ResponseCode.SUCCESS,
+                      responseMessage.SUCCESS,
+                      records);
+
+  res.send(response);
 });
 
 export default router;
