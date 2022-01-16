931ec3ba3e61f1b878d6e63011d623ed5fdef869
Edgar Chirivella <edgar.95.12@gmail.com>
2022-01-16 15:12:56 +0100

    Move service creation in test to before each

--- __tests__/services/searchServiceImpl.spec.ts
+++ __tests__/services/searchServiceImpl.spec.ts
@@ -1,12 +1,15 @@
-import RecordsRepository from "../../src/repositories/recordsRepository";
-import MongoDbClient from "../../src/database/mongoDbClient";
-import SearchServiceImpl from "../../src/services/searchServiceImpl";
+import RecordsRepository from '../../src/repositories/recordsRepository';
+import MongoDbClient from '../../src/database/mongoDbClient';
+import SearchServiceImpl from '../../src/services/searchServiceImpl';
 
 import RecordsRepositoryMongo from '../../src/repositories/recordsRepositoryMongo';
+import SearchService from '../../src//services/searchService';
 jest.mock('../../src/repositories/recordsRepositoryMongo');
 jest.mock('../../src/database/mongoDbClient');
 
 describe('Search service', () => {
+  let service: SearchService;
+
   let mongoClient: MongoDbClient;
   let recordsRepository: RecordsRepository;
 
@@ -16,9 +19,12 @@ describe('Search service', () => {
     (recordsRepository.searchItems as jest.Mock).mockReturnValue(jest.fn());
   });
 
+  beforeEach(() => {
+    service = new SearchServiceImpl(recordsRepository);
+  });
+
   it('should call repository to find items', () => {
     // Arrange
-    const service = new SearchServiceImpl(recordsRepository);
     const startDate = '1970-01-01';
     const endDate = '2022-01-16';
     const minCount = 0;
